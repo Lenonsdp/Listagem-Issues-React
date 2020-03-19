7f6db435b642fa75b2a554ac3191a740f27fcb9d
Lenon S. De Paula <lenonsdp@hotmail.com>
2020-03-18 22:43:00 -0300

    Utilização de axios e aniamação em button

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,28 +1,71 @@
-import React from 'react';
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import React, { Component } from 'react';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles'
-export default function Main() {
-    return (
-        <Container>
-            <h1>
-                <FaGithub/>
-                Repositórios
-            </h1>
 
-            <Form onSubmit={() => {}}>
-                <input
-                    type="text"
-                    placeholder="Adicionar repósitorio"
-                />
+import api from '../../services/api';
 
-                <SubmitButton  disable>
-                    <FaPlus color="#fff" size={14} />
-                </SubmitButton >
+export default class Main extends Component {
+    state = {
+        newRepo: '',
+        repositories: [],
+        loading: false
+    };
 
-            </Form>
-        </Container>
-    );
+    handleInputChange = e => {
+        this.setState({ newRepo: e.target.value });
+    };
 
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        this.setState({ loading: true });
+
+        const { newRepo, repositories } = this.state;
+
+        const response = await api.get(`/repos/${newRepo}`)
+
+        const data = {
+            name: response.data.full_name
+        };
+
+        this.setState({
+            repositories: [ ...repositories, data],
+            newRepo: '',
+            loading: false
+         });
+    };
+
+    render () {
+
+        const { newRepo, loading } = this.state;
+        return (
+            <Container>
+                <h1>
+                    <FaGithub/>
+                    Repositórios
+                </h1>
+
+                <Form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Adicionar repósitorio"
+                        value={newRepo}
+                        onChange={this.handleInputChange}
+                    />
+
+                    <SubmitButton loading={loading ? 1 : 0}>
+                        { loading ? ( 
+                            <FaSpinner color="#fff" size={14} /> 
+                        ) : (
+                            <FaPlus color="#fff" size={14} />
+                        )}
+                        
+                    </SubmitButton >
+
+                </Form>
+            </Container>
+        );
+    }
 }
 
 
